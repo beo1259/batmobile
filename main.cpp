f1ef5d8c4e1052154cf9d1df1729dbd42afd6f8e
beo1259 <oneilb123@gmail.com>
2024-06-29 06:58:13 -0600

    first commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <cstdlib>
+#include <stdio.h>
+#include <iostream>
+#include <pigpio.h>
+
+using namespace std;
+
+// RIGHT BACK MOTOR  
+int m1[] = {23, 24};
+// LEFT BACK MOTOR  
+int m2[] = {25, 18};
+
+// RIGHT FRONT MOTOR  
+int m3[] = {4, 17};
+// LEFT FRONT MOTOR  
+int m4[] = {27, 22};
+
+int main() {
+   
+    gpioTerminate();
+    return 0;
+}
+
